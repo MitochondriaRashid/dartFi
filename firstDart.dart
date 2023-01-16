80ca5f568ad6f7d421bd32f121916ae2c5ccea3f
Rashid Sultanov <resid.sultanov18@gmail.com>
2023-01-16 13:40:01 +0400

    newChanges

--- firstDart.dart
+++ firstDart.dart
@@ -2,6 +2,8 @@ final String newUser = 'Rashid';
 int userMoney = 29;
 int userMin = 5;
 int range = 10;
+
+// aklsdaskdjasd'jsa'djaslkdjaslkdjasdklsa
 void main() {
   actionModel(userMoney, userMin);
 }
