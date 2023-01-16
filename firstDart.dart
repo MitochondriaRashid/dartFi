7f46d0eee41aa9e1ca94eb3363c3f6e8f593480a
Rashid Sultanov <resid.sultanov18@gmail.com>
2023-01-16 13:25:47 +0400

    First Dart project

---
+++ firstDart.dart
@@ -0,0 +1,20 @@
+final String newUser = 'Rashid';
+int userMoney = 29;
+int userMin = 5;
+int range = 10;
+void main() {
+  actionModel(userMoney, userMin);
+}
+
+void actionModel(int userActionValue, int userActionMin) {
+  if (userActionValue > userActionMin) {
+    double functionValueNew = secondFunction();
+    print('$functionValueNew you are excused :)');
+  } else {
+    print('User $newUser sorry :(');
+  }
+}
+
+double secondFunction() {
+  return userMoney / range;
+}
