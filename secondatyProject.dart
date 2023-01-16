3a9614f4d5874fc3b5a93e0dae22031959e4f727
Rashid Sultanov <resid.sultanov18@gmail.com>
2023-01-16 13:54:35 +0400

    secondatyProject

---
+++ secondatyProject.dart
@@ -0,0 +1,11 @@
+void main() {
+  final String project = 'Hi Ai era';
+
+  List<int> arithmeticList = List.generate(21, (Index) {
+    return Index + 0;
+  });
+
+  for (int i = 0; i < arithmeticList.length; i++) {
+    print(arithmeticList[i]);
+  }
+}
